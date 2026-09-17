d5baa25c8ac0d3290a02d9a56df4c844554a3c64
Ankit Kumar <ankitks6688@gmail.com>
2026-09-17 23:39:19 +0530

    agent

--- src/main/java/com/itagent/it_support_agent/controller/AdminIssueController.java
+++ src/main/java/com/itagent/it_support_agent/controller/AdminIssueController.java
@@ -1,5 +1,9 @@
 package com.itagent.it_support_agent.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/issues")
 public class AdminIssueController {
 
 }
