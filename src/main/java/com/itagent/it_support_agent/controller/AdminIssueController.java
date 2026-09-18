9d49f6e9b5c0e96d3726df4431ff4be1f7726261
Ankit Kumar <ankitks6688@gmail.com>
2026-09-18 20:57:50 +0530

    agent

--- src/main/java/com/itagent/it_support_agent/controller/AdminIssueController.java
+++ src/main/java/com/itagent/it_support_agent/controller/AdminIssueController.java
@@ -1,9 +1,31 @@
 package com.itagent.it_support_agent.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.itagent.it_support_agent.dto.IssueRequest;
+import com.itagent.it_support_agent.model.TroubleshootingIssue;
+import com.itagent.it_support_agent.service.AdminIssueService;
 
 @RestController
 @RequestMapping("/admin/issues")
 public class AdminIssueController {
 
+    private final AdminIssueService adminIssueService;
+
+    public AdminIssueController(AdminIssueService adminIssueService) {
+        this.adminIssueService = adminIssueService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TroubleshootingIssue> createIssue(
+            @RequestBody IssueRequest request) {
+
+        TroubleshootingIssue savedIssue =
+                adminIssueService.createIssue(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedIssue);
+    }
 }
