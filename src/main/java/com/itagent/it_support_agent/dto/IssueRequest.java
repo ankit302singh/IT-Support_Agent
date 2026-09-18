873c5ae9a3ab2f083542c2a4e63fa67a52380d0a
Ankit Kumar <ankitks6688@gmail.com>
2026-09-18 11:24:24 +0530

    agent'

--- src/main/java/com/itagent/it_support_agent/dto/IssueRequest.java
+++ src/main/java/com/itagent/it_support_agent/dto/IssueRequest.java
@@ -1,5 +1,56 @@
 package com.itagent.it_support_agent.dto;
 
+import java.util.List;
+
 public class IssueRequest {
 
+    private String issue;
+
+    private List<String> keywords;
+
+    private String solution;
+
+    private String driverName;
+
+    private String driverUrl;
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+
+    public List<String> getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getDriverUrl() {
+        return driverUrl;
+    }
+
+    public void setDriverUrl(String driverUrl) {
+        this.driverUrl = driverUrl;
+    }
 }
